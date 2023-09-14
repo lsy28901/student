310d38d014d960bc1475b31c92b454f4b58b1fdf
lsy28901 <tndus28901@gmail.com>
2023-09-14 17:31:52 +0900

    student project

---
+++ Student.java
@@ -0,0 +1,15 @@
+
+
+// lombok 적용
+public class Student {
+	private String name;
+	private String department;
+	private String id;
+	private double grade;
+	
+	//생성자 추가
+	
+	
+	
+	// getter/setter 추가
+}
---
+++ StudentTest.java
@@ -0,0 +1,6 @@
+class StudentTest{
+	public static void main (String[] args) {
+		StudentManager man = new StudentManager();
+		man.run();
+	}
+}
